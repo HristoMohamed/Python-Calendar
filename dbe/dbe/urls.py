ff4a5abbfaf583f2f57ef85714eaca57484bdd25
asdasd <asdasd@asdasd.(none)>
2013-07-05 16:50:50 +0300

    "committing my project bruh"w

---
+++ dbe/dbe/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, include, url
+from cal.models import *
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+#admin.autodiscover()
+
+urlpatterns = patterns('cal.views',
+    # Examples:
+    # url(r'^$', 'dbe.views.home', name='home'),
+    # url(r'^dbe/', include('dbe.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    (r"^month/(\d+)/(\d+)/(prev|next)/", "month"),
+    (r"^month/(\d+)/(\d+)/", "month"),
+    (r"^month$", "month"),
+    (r"^day/(\d+)/(\d+)/(\d+)/", "day"),
+    (r"^settings/", "settings"),
+    (r"^(\d+)/", "main"),
+    (r"", "main"),
+
+)
